8b9aaa5cbe014af2060820b4ada26bca6b20ec8c
saddamhr <saddambubt65@gmail.com>
2020-10-16 03:48:38 +0600

    custom validation

--- playground/validator.js
+++ playground/validator.js
@@ -15,10 +15,29 @@ router.post('/validator',
             .withMessage('Username cant not be grater than 15 character'),
         check('email')
             .isEmail()
-            .withMessage('Please provide a valid email')
+            .withMessage('Please provide a valid email'),
+            check('password').custom(value => {
+                if(value.length < 5) {
+                    throw new Error('Password must be greater than 5 character')
+                }
+                return true
+            }),
+            check('confirmPassword').custom((value, {req}) => {
+                if(value !== req.body.password) {
+                    throw new Error('Password does not match')
+                }
+                return true
+            })
     ], (req, res, next) => {
         let errors = validationResult(req)
-        console.log(errors)
+
+        const formatter = (error) => error.msg
+
+        console.log(errors.isEmpty())
+        console.log(errors.array())
+        console.log(errors.mapped())
+        
+        console.log(errors.formatWith(formatter).mapped())
         res.render('playground/signup', { title: 'Validator Playground' })
     })
 
